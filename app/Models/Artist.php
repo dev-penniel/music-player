c7d3b02c5f06d2225a54457dba9b5c7fd705533c
= <=>
2026-02-10 22:01:06 +0200

    artist model

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artist extends Model
 {
-    //
+
+    protected $fillable = [
+        'user_id',
+        'stage_name',
+        'slug',
+        'bio',
+        'profile_image',
+        'cover_image',
+        'genre',
+        'location',
+        'is_verified',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function albums()
+    {
+        return $this->hasMany(Album::class);
+    }
+
+    public function tracks()
+    {
+        return $this->hasMany(Track::class);
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'artist_followers');
+    }
 }
+
