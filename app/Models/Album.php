a2fb98ee77a5337b7f88a1a6b09cec8d0bb79118
= <=>
2026-02-10 22:06:04 +0200

    album model

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
-    //
+
+    protected $fillable = [
+        'artist_id',
+        'title',
+        'slug',
+        'cover_image',
+        'type',
+        'release_date',
+        'is_published',
+    ];
+
+    public function artist()
+    {
+        return $this->belongsTo(Artist::class);
+    }
+
+    public function tracks()
+    {
+        return $this->hasMany(Track::class);
+    }
 }
